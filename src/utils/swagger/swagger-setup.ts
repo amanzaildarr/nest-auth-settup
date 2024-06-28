d3e8c91fe067b19eed64ba159fe95eb576fe2809
Amandeep Singh <amanbaidwan3004@gmail.com>
2024-06-28 19:30:16 +0530

    swagger fix

--- src/utils/swagger/swagger-setup.ts
+++ src/utils/swagger/swagger-setup.ts
@@ -10,24 +10,24 @@ const tsCssPath = `${process.cwd()}/src/utils/swagger/custom.css`;
 const jsCssPath = `${process.cwd()}/dist/utils/swagger/custom.css`;
 
 const options = new DocumentBuilder()
-  .setTitle('DrSignet API')
+  .setTitle('Pest-Easy API')
   .setDescription(
-    'Comprehensive documentation for the DrSignet APIs.\n\n' +
+    'Comprehensive documentation for the Pest-Easy APIs.\n\n' +
       'This documentation provides detailed information and interactive access to all available endpoints, ' +
-      'helping developers integrate with the DrSignet platform effectively.\n\n' +
-      'For more information, visit our [website](https://www.drsignet.com) or check our ' +
-      '[GitHub repository](https://github.com/drsignet).\n\n' +
+      'helping developers integrate with the Pest-Easy platform effectively.\n\n' +
+      'For more information, visit our [website](https://www.pest-easy.com) or check our ' +
+      '[GitHub repository](https://github.com/pest-easy).\n\n' +
       '### Key Features\n' +
       '- Secure and efficient authentication\n' +
       '- Comprehensive CRUD operations\n' +
       '- Detailed analytics and reporting\n\n' +
       '### Contact\n' +
-      'For support, please contact our team at [support@drsignet.com](mailto:support@drsignet.com).\n\n' +
+      'For support, please contact our team at [support@pest-easy.com](mailto:support@pest-easy.com).\n\n' +
       '### License\n' +
-      'This API is licensed under the MIT License. See the [LICENSE](https://github.com/drsignet/license) file for more details.',
+      'This API is licensed under the MIT License. See the [LICENSE](https://github.com/pest-easy/license) file for more details.',
   )
   .setVersion('1.0.1')
-  .setTermsOfService('https://www.drsignet.com/terms')
+  .setTermsOfService('https://www.pest-easy.com/terms')
   .addBearerAuth(
     { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' },
     'access-token',
@@ -41,7 +41,7 @@ async function setupSwagger(app: INestApplication) {
   await copyFile(tsCssPath, jsCssPath);
 
   const customOptions: SwaggerCustomOptions = {
-    customSiteTitle: 'DrSignet API Documentation',
+    customSiteTitle: 'Pest-Easy API Documentation',
     swaggerOptions: {
       docExpansion: 'none', // Collapse all sections by default
     },
