f7424422194a2c989b8bce3f1afc6bc054c547cf
Amandeep Singh <amanbaidwan3004@gmail.com>
2024-06-28 19:31:28 +0530

    health check

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiExcludeEndpoint } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
+  @Get('health')
+  @ApiExcludeEndpoint()
+  healthCheck() {
     return this.appService.getHello();
+  }
+
+  @Get('environment')
+  @ApiExcludeEndpoint()
+  getEnv() {
+    return this.appService.getEnv();
   }
 }
