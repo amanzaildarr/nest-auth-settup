3f90adbacc76e17dfec20b5d233d2c8567716076
Amandeep Singh <amanbaidwan3004@gmail.com>
2024-06-30 09:54:36 +0530

    auth

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,28 @@
+import { Global, Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { UserService } from 'src/user/user.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from 'src/user/schema/user.schema';
+
+@Global()
+@Module({
+  imports: [
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: configService.get<string | number>('JWT_EXPIRE'),
+        },
+      }),
+    }),
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, UserService],
+  exports: [AuthService, UserService, JwtModule],
+})
+export class AuthModule {}
